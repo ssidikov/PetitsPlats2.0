407f68bae1be2e23a4b9cec1bfda6e192608b65d
ssidikov <sardorbek.sidikov7@gmail.com>
2024-06-13 00:35:52 +0200

    Update recipe card creation and add options for appliances, utensils, and ingredients

--- scripts/factory/recipeCardFactory.js
+++ scripts/factory/recipeCardFactory.js
@@ -1,5 +1,5 @@
 export function createRecipeCard(data) {
-  const { id, image, name, ingredients, time, description } = data;
+  const { id, image, name, ingredients, appliance, ustensils, time, description } = data;
 
   const imageSrc = `./assets/images/${image}`;
 
@@ -9,6 +9,45 @@ export function createRecipeCard(data) {
   card.id = id;
   card.classList.add("recipes-card", "col-md-4");
 
+  // Appliances options
+  const appliancesOptions = (appliance) => {
+    let result = "";
+
+    for (let i = 0; i < appliance.length; i++) {
+      result += `<option value="${appliance}">${appliance}</option>`;
+    }
+    return result;
+  };
+
+  const appliancesListOptions = document.getElementById("appliances-list");
+  appliancesListOptions.innerHTML += appliancesOptions(appliance);
+
+  // Ustenlice options
+  const ustensilsOptions = (ustensils) => {
+    let result = "";
+
+    for (let i = 0; i < ustensils.length; i++) {
+      result += `<option value="${ustensils[i]}">${ustensils[i]}</option>`;
+    }
+    return result;
+  };
+
+  const ustensilsListOptions = document.getElementById("utensils-list");
+  ustensilsListOptions.innerHTML += ustensilsOptions(ustensils);
+  // Ingredients options
+  const ingredientsOptions = (ingredients) => {
+    let result = "";
+
+    for (let i = 0; i < ingredients.length; i++) {
+      result += `<option value="${ingredients[i].ingredient}">${ingredients[i].ingredient}</option>`;
+    }
+    return result;
+  };
+
+  const ingredientsListOptions = document.getElementById("ingredients-list");
+  ingredientsListOptions.innerHTML += ingredientsOptions(ingredients);
+
+  // Ingredients list for each recipe
   const ingredientsList = (ingredients) => {
     let result = "";
 
