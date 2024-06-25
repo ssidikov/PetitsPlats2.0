1fd3678dce8d8056648df2996082528e3b50d704
ssidikov <sardorbek.sidikov7@gmail.com>
2024-06-26 00:56:40 +0200

    refactor: Improve dropdown functionality and styles

--- scripts/components/dropdown.js
+++ scripts/components/dropdown.js
@@ -1,46 +1,45 @@
+// dropdown.js
+
 export function initDropdowns() {
   const dropdownButtons = document.querySelectorAll(".dropdown-toggle");
 
   dropdownButtons.forEach((button) => {
-    button.addEventListener("click", function () {
-      const dropdownMenu = button.nextElementSibling;
-      const input = dropdownMenu.querySelector(".form-control");
-
-      if (dropdownMenu.classList.contains("show")) {
-        closeDropdown(button, dropdownMenu);
-      } else {
-        closeAllDropdowns(); // Close all other dropdowns before opening this one
-
-        openDropdown(button, dropdownMenu, input);
-      }
-    });
+    button.addEventListener("click", () => toggleDropdown(button));
   });
 
-  function openDropdown(button, dropdownMenu, input) {
+  function toggleDropdown(button) {
+    const dropdownMenu = button.nextElementSibling;
+
+    if (dropdownMenu.classList.contains("show")) {
+      closeDropdown(button, dropdownMenu);
+    } else {
+      closeAllDropdowns();
+      openDropdown(button, dropdownMenu);
+    }
+  }
+
+  function openDropdown(button, dropdownMenu) {
+    const input = dropdownMenu.querySelector(".form-control");
+
     dropdownMenu.classList.add("show");
     button.classList.add("open");
     button.setAttribute("aria-expanded", "true");
 
     if (input) {
       input.focus();
-      input.value = ""; // Clear input value on dropdown open
-
+      input.value = "";
       applyFilter(input, dropdownMenu);
     }
   }
 
   function applyFilter(input, dropdownMenu) {
-    input.addEventListener("input", function () {
+    input.addEventListener("input", () => {
       const filter = input.value.trim().toLowerCase();
       const items = dropdownMenu.querySelectorAll(".dropdown-item");
 
       items.forEach((item) => {
         const text = item.textContent.trim().toLowerCase();
-        if (text.includes(filter)) {
-          item.style.display = "";
-        } else {
-          item.style.display = "none";
-        }
+        item.style.display = text.includes(filter) ? "" : "none";
       });
     });
   }
@@ -49,21 +48,20 @@ export function initDropdowns() {
     dropdownMenu.classList.remove("show");
     button.classList.remove("open");
     button.setAttribute("aria-expanded", "false");
-
     resetFilter(dropdownMenu);
   }
 
   function resetFilter(dropdownMenu) {
     const items = dropdownMenu.querySelectorAll(".dropdown-item");
     items.forEach((item) => {
-      item.style.display = ""; // Reset display to default
+      item.style.display = "";
     });
   }
 
   function closeAllDropdowns() {
     document.querySelectorAll(".dropdown-menu").forEach((menu) => {
       menu.classList.remove("show");
-      resetFilter(menu); // Reset filter for all menus
+      resetFilter(menu);
     });
     document.querySelectorAll(".dropdown-toggle").forEach((btn) => {
       btn.classList.remove("open");
@@ -80,16 +78,13 @@ export function generateOptions(items) {
   const uniqueItems = [...new Set(items)];
   const sortedItems = sortAlphabetically(uniqueItems);
   return sortedItems
-    .map((item) => `<li><a class="dropdown-item" href="#" tabindex="0">${item}</a></li>`)
+    .map((item) => `<li><a class="dropdown-item" href="#">${item}</a></li>`)
     .join("");
 }
 
 export function appendDropdownOptions(selector, options) {
   const element = document.getElementById(selector);
   if (element) {
-    // const inputElement = element.querySelector("input");
-
-    // Remove all elements after the search input
     const ulElement = element.querySelector(".dropdown-options");
     ulElement.innerHTML = options;
   }
@@ -106,7 +101,6 @@ export function getUniqueOptions(recipes) {
     recipe.ustensils.forEach((ut) => utensils.add(ut.toLowerCase()));
   });
 
-  // Return unique options as arrays
   return {
     ingredients: [...ingredients],
     appliances: [...appliances],
