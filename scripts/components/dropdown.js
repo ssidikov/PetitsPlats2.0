3f9c21a2db7b421031e8b297264130e2f728db6e
ssidikov <sardorbek.sidikov7@gmail.com>
2024-06-27 21:06:40 +0200

    refactor: Update dropdown options and accessibility

--- scripts/components/dropdown.js
+++ scripts/components/dropdown.js
@@ -101,6 +101,52 @@ export function initDropdowns() {
   });
 }
 
+// Data for filters
+const filters = [
+  { id: "ingredients", label: "Ingrédients" },
+  { id: "appliances", label: "Appareils" },
+  { id: "utensils", label: "Ustensiles" },
+];
+
+// Function for creating filters
+export function createFilterOptions() {
+  const filterBarOptions = document.querySelector(".filter-bar__options");
+  filters.forEach((filter) => {
+    const filterOption = document.createElement("div");
+    filterOption.classList.add("filter-options", "dropdown");
+
+    filterOption.innerHTML = `
+      <button class="filter-button dropdown-toggle" type="button" id="${
+        filter.id
+      }Dropdown" aria-expanded="false" aria-haspopup="true">
+        ${filter.label}
+        <i class="bi bi-chevron-down" aria-hidden="true"></i>
+      </button>
+      <div class="dropdown-menu" aria-labelledby="${filter.id}Dropdown" id="${filter.id}-list">
+        <div class="sticky">
+          <label for="${
+            filter.id
+          }-search" class="visually-hidden">Rechercher ${filter.label.toLowerCase()}</label>
+          <input id="${
+            filter.id
+          }-search" type="text" class="form-control mb-2" placeholder="Rechercher...">
+          <button class="form-control__icon-clear" type="reset" aria-label="Options search clear button">
+            <i class="bi bi-x" aria-hidden="true"></i>
+          </button>
+          <button class="form-control__icon-search" type="submit" aria-label="Options search button">
+            <i class="bi bi-search" aria-hidden="true"></i>
+          </button>
+        </div>
+        <ul class="dropdown-options" role="listbox" aria-labelledby="${filter.id}Dropdown">
+          <!-- dynamic options will be appended here -->
+        </ul>
+      </div>
+    `;
+
+    filterBarOptions.appendChild(filterOption);
+  });
+}
+
 // Function to sort items alphabetically
 function sortAlphabetically(array) {
   return array.sort((a, b) => a.localeCompare(b));
